ecff42dced4c2b6a2f9fe69b0903f22be0b93007
Phil Peble <phil@peble.net>
2026-08-29 01:22:23 -0500

    Update remaining LoadAllDefinitions references to Bootstrap
    Signed-off-by: Phil Peble <phil@peble.net>

--- test/e2e/test_all_regions.rb
+++ test/e2e/test_all_regions.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.dirname(__FILE__)) + '/../test_helper'
 
 class AllRegionsTests < Test::Unit::TestCase
   def setup
-    Holidays::LoadAllDefinitions.call
+    Holidays::Bootstrap.call
   end
 
   def test_definition_dir
